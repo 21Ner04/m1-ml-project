52e4b0f79387e7b5cfe03412a93d62b7e31d7635
21Ner04 <ttobievna@yandex.ru>
2023-11-26 15:55:13 +0200

    js

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ function getUserMediaSupported() {
   return !!(navigator.mediaDevices &&
     navigator.mediaDevices.getUserMedia);
 }
+
+// If webcam supported, add event listener to button for when user
+// wants to activate it to call enableCam function which we will 
+// define in the next step.
+if (getUserMediaSupported()) {
+  enableWebcamButton.addEventListener('click', enableCam);
+} else {
+  console.warn('getUserMedia() is not supported by your browser');
+}
