7e0e69de0762e10854d823b4cb608cc4524e2b26
21Ner04 <ttobievna@yandex.ru>
2023-11-26 15:54:50 +0200

    js

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Making a smart webcam in JavaScript with a TensorFlow.js pre-trained Machine Learning model - course from codelabs.developers.google.com
+Making a smart webcam in JavaScript with a TensorFlow.js pre-trained Machine Learning model - course from codelabs.developers.google.com
---
+++ script.js
@@ -0,0 +1,10 @@
+const video = document.getElementById('webcam');
+const liveView = document.getElementById('liveView');
+const demosSection = document.getElementById('demos');
+const enableWebcamButton = document.getElementById('webcamButton');
+
+// Check if webcam access is supported.
+function getUserMediaSupported() {
+  return !!(navigator.mediaDevices &&
+    navigator.mediaDevices.getUserMedia);
+}
